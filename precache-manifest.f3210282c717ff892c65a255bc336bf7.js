f3cc303e9d6ee7a0e5e91a6d763c85e6d5818db6
Jovany Steven Navares <jovany.steven.navares@gmail.com>
2019-07-25 22:02:03 +0800

    Updates

---
+++ precache-manifest.f3210282c717ff892c65a255bc336bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53507f51a351b0e34e681e6f23357af6",
+    "url": "/willie-labastida/index.html"
+  },
+  {
+    "revision": "24105b58a064aa589c57",
+    "url": "/willie-labastida/static/css/main.95c025df.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/willie-labastida/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "24105b58a064aa589c57",
+    "url": "/willie-labastida/static/js/main.dd5851c3.chunk.js"
+  },
+  {
+    "revision": "e8bc0855339d3cca9d69",
+    "url": "/willie-labastida/static/js/runtime~main.a73b9896.js"
+  },
+  {
+    "revision": "55b3ec6a464f8ede06f06c52b81cc662",
+    "url": "/willie-labastida/static/media/banner.55b3ec6a.jpg"
+  }
+]);
